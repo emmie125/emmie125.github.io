500b3d3ea5b414d15a3ae5a01c14c60ae94ca9f1
emmie125 <mulangaemmanuella@gmail.com>
2021-04-07 11:45:08 +0200

    nineth commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-const identity;
+const avatarProfil= document.querySelector('.header__profil__image img');
+const noms = document.querySelector('.header__profil__title h1');
+const biography = document.querySelector('.section__acceuil p')
+
 window.addEventListener('load',()=>{
-    fetch("https://emmie125.github.io/Data/data.json/").then((response)=>{
+    fetch("https://my-json-server.typicode.com/emmie125/emmie125.github.io/identities").then((response)=>{
         return response.json();
-    })
+    }).then((response)=>{
+        for (const identity of response) {
+            noms.textContent = identity.identity;
+            avatarProfil.setAttribute('src',identity.avatarprofil);
+            biography.textContent =identity.biography;
+        }
+        console.log(response);
+    });
 })
